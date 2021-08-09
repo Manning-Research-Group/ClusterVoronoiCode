e518076cae0723eda11494401aa9de003e5af119
elawsonk <elawsonk@syr.edu>
2021-08-09 12:58:04 -0400

    Cleaned up files

--- vor200l.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash                                                                                  
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/elawsonk/cudalibs:/usr/local/cuda/lib64:/usr/local/lib64
-
-/home/elawsonk/cellGPU-master/voronoi_200_large.out
