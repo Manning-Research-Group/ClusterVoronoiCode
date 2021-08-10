1b687d81ed1f709f27cbf6166deced25a7fd99b8
elawsonk <elawsonk@syr.edu>
2021-08-10 13:53:28 -0400

    Updated with switch - Deleted Sub Files

--- cluster_advect500_25_1_1_5_27.0_1_0.001.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash                                                                                  
-            export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/elawsonk/cudalibs:/usr/local/cuda/lib64:/usr/local/lib64
-
-            /home/elawsonk/cellGPU-master/voronoi_cluster.out -n 500 -v 1 -w 25 -h 27.0 -e 0.001 -t 100000.0 -i 1000.0
--- cluster_advect500_40_1_1_5_28.0_1_0.001.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash                                                                                  
-            export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/elawsonk/cudalibs:/usr/local/cuda/lib64:/usr/local/lib64
-
-            /home/elawsonk/cellGPU-master/voronoi_cluster.out -n 500 -v 1 -w 40 -h 28.0 -e 0.001 -t 100000.0 -i 1000.0
